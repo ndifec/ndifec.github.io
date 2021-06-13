cc778b354512376a0948b459b1da73abe9ba2a29
ndifec <36167028+ndifec@users.noreply.github.com>
2021-06-13 19:44:06 -0400

    iglucose to api

---
+++ monitorIntegration/Models/monitorIntegrationItem.cs
@@ -0,0 +1,60 @@
+using System;
+using Newtonsoft.Json;
+
+namespace monitorIntegration.Models
+{
+    [JsonObject]
+    public class monitorInformationItem
+    {
+        public long id {get; set;}
+
+        [JsonProperty("reading_id")]
+        public long reading_id {get; set;}
+
+        [JsonProperty("irregular")]
+        public bool irregular {get; set;}
+
+        [JsonProperty("device_model")]
+        public string device_model {get; set;}
+
+        [JsonProperty("date_received")]
+        public DateTime date_received {get; set;}
+
+        [JsonProperty("blood_glucose_mgdl")]
+        public Double blood_glucose_mgdl {get; set;}
+
+        [JsonProperty("before_meal")]
+        public bool before_meal {get; set;}
+
+        [JsonProperty("pulse_bpm")]
+        public int pulse_bpm {get; set;}
+
+        [JsonProperty("Rate")]
+        public int Rate {get; set;}
+
+        [JsonProperty("date_recorded")]
+        public DateTime date_recorded {get; set;}
+
+        [JsonProperty("systolic_mmhg")]
+        public int systolic_mmhg {get; set;}
+
+        [JsonProperty("battery")]
+        public int battery {get; set;}
+
+        [JsonProperty("time_zone_offset")]
+        public Double time_zone_offset {get; set;}
+
+        [JsonProperty("reading_type")]
+        public string reading_type {get; set;}
+
+        [JsonProperty("event_flag")]
+        public string event_flag {get; set;}
+
+        [JsonProperty("Weight")]
+        public int Weight {get; set;}
+
+        [JsonProperty("device_id")]
+        public string device_id {get; set;}
+
+    }
+}
--- monitorIntegration/Startup.cs
+++ monitorIntegration/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using monitorIntegration.Models;
 
 namespace monitorIntegration
 {
@@ -26,12 +20,10 @@ namespace monitorIntegration
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<monitorIntegrationContext>(opt =>
+                                                            opt.UseInMemoryDatabase("MonitorIntegrationList"));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "monitorIntegration", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +32,7 @@ namespace monitorIntegration
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "monitorIntegration v1"));
+               
             }
 
             app.UseHttpsRedirection();
