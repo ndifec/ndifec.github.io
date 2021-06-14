23ecbc8f497994e810381713e08e0aa566b40b14
ndifec <36167028+ndifec@users.noreply.github.com>
2021-06-13 21:21:29 -0400

    remove unused using

--- monitorIntegration/Controllers/monitorIntegrationItemsController.cs
+++ monitorIntegration/Controllers/monitorIntegrationItemsController.cs
@@ -2,7 +2,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using monitorIntegration.Models;
-using Newtonsoft.Json;
 
 namespace monitorIntegration.Controllers
 {
