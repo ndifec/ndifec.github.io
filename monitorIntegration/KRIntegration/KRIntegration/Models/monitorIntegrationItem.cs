a2516543ffa216cf045485196289d63a44068219
ndifec <36167028+ndifec@users.noreply.github.com>
2022-07-20 21:19:29 -0400

    Update monitorIntegrationItem.cs

--- monitorIntegration/KRIntegration/KRIntegration/Models/monitorIntegrationItem.cs
+++ monitorIntegration/KRIntegration/KRIntegration/Models/monitorIntegrationItem.cs
@@ -6,8 +6,6 @@ namespace monitorIntegration.Models
     [JsonObject]
     public class monitorInformationItem
     {
-        public long id {get; set;}
-
         [JsonProperty("reading_id")]
         public long reading_id {get; set;}
 
@@ -20,17 +18,8 @@ namespace monitorIntegration.Models
         [JsonProperty("date_received")]
         public DateTime date_received {get; set;}
 
-        [JsonProperty("blood_glucose_mgdl")]
-        public Double blood_glucose_mgdl {get; set;}
-
-        [JsonProperty("before_meal")]
-        public bool before_meal {get; set;}
-
         [JsonProperty("pulse_bpm")]
         public int pulse_bpm {get; set;}
-
-        [JsonProperty("Rate")]
-        public int Rate {get; set;}
 
         [JsonProperty("date_recorded")]
         public DateTime date_recorded {get; set;}
@@ -41,20 +30,14 @@ namespace monitorIntegration.Models
         [JsonProperty("diastolic_mmhg")]
         public int diastolic_mmhg { get; set; }
 
-        [JsonProperty("battery")]
-        public int battery {get; set;}
-
         [JsonProperty("time_zone_offset")]
         public Double time_zone_offset {get; set;}
 
         [JsonProperty("reading_type")]
         public string reading_type {get; set;}
 
-        [JsonProperty("event_flag")]
+        [JsonProperty("short_code")]
         public string event_flag {get; set;}
-
-        [JsonProperty("Weight")]
-        public int Weight {get; set;}
 
         [JsonProperty("device_id")]
         public string device_id {get; set;}
