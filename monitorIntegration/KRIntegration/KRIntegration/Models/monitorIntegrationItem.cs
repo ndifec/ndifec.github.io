96d446148221bbeeb2c4baf5723a1f876f2d8048
ndifec <36167028+ndifec@users.noreply.github.com>
2022-07-20 21:25:09 -0400

    Revert "Update monitorIntegrationItem.cs"
    This reverts commit a2516543ffa216cf045485196289d63a44068219.

--- monitorIntegration/KRIntegration/KRIntegration/Models/monitorIntegrationItem.cs
+++ monitorIntegration/KRIntegration/KRIntegration/Models/monitorIntegrationItem.cs
@@ -6,6 +6,8 @@ namespace monitorIntegration.Models
     [JsonObject]
     public class monitorInformationItem
     {
+        public long id {get; set;}
+
         [JsonProperty("reading_id")]
         public long reading_id {get; set;}
 
@@ -18,8 +20,17 @@ namespace monitorIntegration.Models
         [JsonProperty("date_received")]
         public DateTime date_received {get; set;}
 
+        [JsonProperty("blood_glucose_mgdl")]
+        public Double blood_glucose_mgdl {get; set;}
+
+        [JsonProperty("before_meal")]
+        public bool before_meal {get; set;}
+
         [JsonProperty("pulse_bpm")]
         public int pulse_bpm {get; set;}
+
+        [JsonProperty("Rate")]
+        public int Rate {get; set;}
 
         [JsonProperty("date_recorded")]
         public DateTime date_recorded {get; set;}
@@ -30,14 +41,20 @@ namespace monitorIntegration.Models
         [JsonProperty("diastolic_mmhg")]
         public int diastolic_mmhg { get; set; }
 
+        [JsonProperty("battery")]
+        public int battery {get; set;}
+
         [JsonProperty("time_zone_offset")]
         public Double time_zone_offset {get; set;}
 
         [JsonProperty("reading_type")]
         public string reading_type {get; set;}
 
-        [JsonProperty("short_code")]
+        [JsonProperty("event_flag")]
         public string event_flag {get; set;}
+
+        [JsonProperty("Weight")]
+        public int Weight {get; set;}
 
         [JsonProperty("device_id")]
         public string device_id {get; set;}
